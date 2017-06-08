69bbc6dd733d3d136254b33c13a7bfc7c446cb83
GodloveD <davidgodlove@gmail.com>
2017-06-07 21:33:46 -0400

    WR's excellennt updates

--- rnaseq_example/build.sh
+++ rnaseq_example/build.sh
@@ -9,26 +9,17 @@ function die {
 }
 
 def_file=${1:-none}
-[[ "$def_file" != "none" ]] || die "USAGE: build.sh deffile imagename"
+[[ "$def_file" != "none" ]] || die "USAGE: build.sh deffile imagename size_mb"
 image_name=${2:-none}
-[[ "$image_name" != "none" ]] || die "USAGE: build.sh deffile imagename"
-# final name of the image; also directory name for bootstrap
+[[ "$image_name" != "none" ]] || die "USAGE: build.sh deffile imagename size_mb"
+# final name of the image
 image=${PWD}/${image_name}
 # temp name for the image file when first created
-tmp=${image}.tmp
-[[ -e "$tmp" || -e "$image" ]] && die "Image file or temp dir already exists"
+[[ -e "$image" ]] && die "Image file already exists"
 
-mkdir $image
-sudo chown root:root $image
-
+# tar was having issues with hard links and hidden files
+size_mb=${3:-0}
+[[ $size_mb -eq 0 ]] && die "please specify image size"
+singularity create -s $size_mb $image
 sudo singularity bootstrap $image $def_file
-size_mb=$(sudo du -ms $image | awk '{print $1 + 200}')
-sudo singularity create -s $size_mb $tmp
-pushd $image
-sudo tar -c ./* | sudo singularity import $tmp
-popd 
 
-# delete the temp dir
-sudo rm -rf $image
-# rename the image file to its final name
-mv $tmp $image
